f7ab18bdcb7e62e710ce3e405c4567b9b5b8f503
Elie Mukau Ambel <s109059@ap.be>
2020-08-31 12:11:28 +0200

    My first commit

--- database.js
+++ database.js
@@ -10,3 +10,16 @@ conn.connect(function(err) {
   console.log('Database is connected successfully !');
 });
 module.exports = conn;
+// const {Client } = require('pg');
+
+// const client = new Client({
+//   user: "kzgnhfmsccbjbj",
+//   host: "ec2-176-34-123-50.eu-west-1.compute.amazonaws.com",
+//   database: "debrurlm7aobrm",
+//   password: "8e290c82e2cbbf7f59e681a3f71ab6435f493136e4d723166a3c23e9134db148",
+//   port: "5432"
+// });
+// client.connect(function(err) {
+//   if (err) throw err;
+//   console.log('Database is connected successfully !');
+//   });
