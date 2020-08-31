124da32dcb3223aa3721e637f08b7a4fb5a65033
Elie Mukau Ambel <s109059@ap.be>
2020-08-31 10:08:40 +0200

    make it better

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
 var createError = require('http-errors');
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var path = require('path');
 var db=require('./database');
+var nodemailer = require("nodemailer");
+var smtpTransport = require('nodemailer-smtp-transport');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var afterRouter = require('./routes/after');
+// **********************************************************************
+app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.json());
+app.use(express.static(process.cwd() + '/public'));
+app.use('/images', express.static(__dirname + '/images'));
 
-app.use(express.static(path.join(__dirname, 'public')));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs', );
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('public'))
 app.use('/', indexRouter);
 app.use('/user', usersRouter);
 app.get('/after', afterRouter);
+
+app.get('/index', function(req, res, next) {
+  res.render('index');
+});
+
 app.get('/checkout-page', function(req, res, next) {
   res.render('checkout-page');
 });
@@ -27,7 +36,15 @@ app.get('/checkout-page', function(req, res, next) {
 app.get('/checkout', function(req, res, next) {
     res.render('checkout');
   });
-  
+
+app.get('/contact', function(req, res, next) {
+    res.render('contact');
+  });
+app.get('/contact-after', function(req, res, next) {
+    res.render('contact-after');
+  });
+  // **********************************************************************
+
   app.post('/checkout-create', function(req, res, next) {
       
     var eenCheckoutNaam     = req.body.Naam;
@@ -40,10 +57,60 @@ app.get('/checkout', function(req, res, next) {
        });
        res.redirect('/checkout-page');
   });
+  // **********************************************************************
+  app.post('/send', function(req, res) {
+    const output = `
+    <p>U hebt een nieuw bericht</p>
+    <h3> Details van de klant</h3>
+    <ul>
+      <li>Voornaam: ${req.body.achternaam}</li>
+      <li>Achternaam: ${req.body.voornaam}</li>
+      <li>E-mailadres: ${req.body.email}</li>
+      <li>Telefoonnummer: ${req.body.telnumber}</li>
+    </ul>
+    <h3>Bericht:</h3>
+    <p>${req.body.onderwerp}</p>
+    `;
+
+    "use strict";
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // Create the transporter with the required configuration for Outlook
+// change the user and pass !
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+      user: 'mukauelie@gmail.com',
+      pass: 'esther1995'
+  }
+});
+
+// setup email data with unicode symbols
+let mailOptions = {
+  from: '"vandaagzitikhier.be" <mukauelie@gmail.com>', // sender address
+  to: 'Unique Websites, uniquewebsites77@hotmail.com', // list of receivers
+  subject: 'Contact openemen', // Subject line
+  text: output, // plain text body
+  html: output // html body
+};
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+      return console.log(error);
+  }
+  console.log('Message %s sent: %s', info.messageId, info.response);
+});
+res.redirect('/contact-after');
+}
+
+main().catch(console.error);
+
+});
+
+
 
   
-
-// catch 404 and forward to error handler
-
 
 module.exports = app;
--- database.js
+++ database.js
@@ -1,4 +1,3 @@
-/*
 var mysql = require('mysql');
 var conn = mysql.createConnection({
   host: 'localhost', // Replace with your host name
@@ -11,16 +10,3 @@ conn.connect(function(err) {
   console.log('Database is connected successfully !');
 });
 module.exports = conn;
-*/
-const {Pool} = require('pg');
-const pool = new Pool({
-  user: 'tfhqovyjoefkkc',
-  host: 'ec2-54-247-79-178.eu-west-1.compute.amazonaws.com',
-  database: 'df6alk7pgpju9i',
-  password: '53120d60cbb844252909ef290cfd7ad0923b4dfbdd5600903f075e8a3a234861',
-  port: '5432'
-})
-pool.query('CREATE TABLE Test', (err, res)=>{
-  console.log(err, res)
-  pool.end();
-})
